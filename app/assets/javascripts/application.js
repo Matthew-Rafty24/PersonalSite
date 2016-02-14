4d08b6cdc6d98bfb4295562e854326d2e9a48e43
Matthew Raft <matthewraft24@outlook.com>
2016-02-14 16:45:59 +0000

    navbar changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', '#f5f2ea');
+          $(".navbar-default").css('background-color', '#000');
        } else {
           $('.navbar-default').css('background-color', 'transparent');
        }
