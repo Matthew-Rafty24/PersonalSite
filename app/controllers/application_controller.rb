0b81e60e16f4e7a31d8449367d984a73662de50d
Matthew Raft <matthewraft24@outlook.com>
2016-02-15 15:15:08 +0000

    cv added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # def download
+  #   send_file '/assets/MatthewCV.pdf', :type=>"application/pdf", :x_sendfile=>true
+  # end
+
 end
